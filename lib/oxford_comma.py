9b8cd4068c53ddd70394f6522a7f1916ed6ec96e
Joy <joyglory213@gmail.com>
2023-11-27 17:10:13 +0300

    completed

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,12 @@
+# oxford_comma.py
+
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return f"{items[0]} and {items[1]}"
+    else:
+        comma = ", ".join(items[:-1])
+        return f"{comma}, and {items[-1]}"
+
+    
